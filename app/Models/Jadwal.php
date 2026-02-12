80939e9cd48a0aa3be10624081ee32e1a39f7d03
heisoyyy <heikelqushoyi@gmail.com>
2026-02-12 21:46:34 +0700

    doneeeee

--- app/Models/Jadwal.php
+++ app/Models/Jadwal.php
@@ -6,10 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Jadwal extends Model
 {
-    protected $fillable = ['tanggal', 'waktu', 'tempat', 'team_id', 'urutan','reminder_sent'];
+    protected $fillable = [
+        'tanggal',
+        'waktu',
+        'tempat',
+        'team_id',
+        'urutan',
+        'reminder_sent'
+    ];
+
+    protected $attributes = [
+        'reminder_sent' => 0,
+    ];
 
     public function team()
     {
         return $this->belongsTo(Team::class, 'team_id');
     }
 }
+
